ba85b031aa33dfe979392d76133d470ec4a0ceb6
fronox <serjzv@gmail.com>
2019-11-20 13:23:44 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -8,23 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/*//Video element used for rendering video self-view
-        <video width="25%"  id="localVideo" autoPlay playsInline  muted="muted"/>
-        //Video element used for rendering video of remote party
-        <video width="50%" id="remoteVideo" autoPlay playsInline />
-
-        <WebRTCClient
-          video={true}
-          autoRegister = {true}
-          sipDomain={decodeURIComponent(this.queryParams.domain)}
-          sipServer={decodeURIComponent(this.queryParams.sipserver)}
-          sipUser={decodeURIComponent(this.queryParams.userid)}
-          sipPassword={decodeURIComponent(this.queryParams.password)}
-          destination={decodeURIComponent(this.queryParams.destination)}
-          metaData={{param1:"value1",obj1:{objparam1:"objvalue1"}}}
-        />*/
-          RecorderCompReact
-        }
+        { RecorderCompReact }
       </header>
     </div>
   );
